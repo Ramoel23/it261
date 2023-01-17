83f2c161e5cff2d8b2c9124e7bfcfdeda8136b44
Ramoel23 <79807374+Ramoel23@users.noreply.github.com>
2023-01-17 00:40:04 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My portal page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+<h1><a herf="index.php">Leo's Portal Page</a></h1>
+<h2>The navigation below represents our big assiment </h2>
+<div> <id="wrapper">
+<nav>
+<ul>
+
+<li><a href="">Switch</a></li>
+<li><a href="">Troubleshoot</a></li>
+<li><a href="">Calculator</a></li>
+<li><a href="">Email</a></li>
+<li><a href="">Database</a></li>
+<li><a href="">Gallery</a></li>
+</ul>
+
+</nav>
+
+
+<main>
+    <h2>About Leo</h2>
+        <p>Hello my is Leomar Aparra. I grew in Hawaii moved around lived in Houston for a few years.Went back to Hawaii In middle school joined the military when i was 17 years old. Got station in Hawaii. after a few years of living there it seemed like a dead end move to vegas was having a little to much fun there. Eventually moved hear to pursue something greater than myself. I have a few Hobbies has any one in this industry I like to tweak on the computer and games. I also Enjoy the outdoors. Im a big gym enthusiast lifthing running just keeping the body moving. Ive also trained in boxing, kick boxing, Muaythai and a littl bit of jujitsu </p>
+        <img class="right" src="images/me.png" alt="Leo">
+
+        <img class="left" src="images/Mamp.png" alt="Mamp">
+
+        <img class="center" src="images/mamperror.png" alt="error">
+
+
+</main>
+
+
+<aside>
+    <h2>weekly class exercies</h2>
+<h3>week2</h3>
+<ul>
+
+<li><a href="weeks/week2/var.php">var.php</a></li>
+<li><a href="weeks/week2/var2.php">var2.php</a></li>
+<li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
+<li><a href="weeks/week2/currency.php">currency.php</a></li>
+
+</ul>
+
+
+</aside>
+
+
+
+
+
+</div>
+
+
+  
+</body>
+</html>
