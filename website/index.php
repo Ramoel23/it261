b16a406c2cbba8b94b7c62581acaaf3f2278cf30
Ramoel23 <79807374+Ramoel23@users.noreply.github.com>
2023-01-23 11:40:54 -0800

    Add files via upload

--- website/index.php
+++ website/index.php
@@ -1,66 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My portal page</title>
-    <link href="css/styles.css" type="text/css" rel="stylesheet">
-</head>
-<body>
-
-<h1><a herf="index.php">Leo's Portal Page</a></h1>
-<h2>The navigation below represents our big assiment </h2>
-<div> <id="wrapper">
-<nav>
-<ul>
-
-<li><a href="">Switch</a></li>
-<li><a href="">Troubleshoot</a></li>
-<li><a href="">Calculator</a></li>
-<li><a href="">Email</a></li>
-<li><a href="">Database</a></li>
-<li><a href="">Gallery</a></li>
-</ul>
-
-</nav>
-
-
-<main>
-    <h2>About Leo</h2>
-        <p>Hello my is Leomar Aparra. I grew in Hawaii moved around lived in Houston for a few years.Went back to Hawaii In middle school joined the military when i was 17 years old. Got station in Hawaii. after a few years of living there it seemed like a dead end move to vegas was having a little to much fun there. Eventually moved hear to pursue something greater than myself. I have a few Hobbies has any one in this industry I like to tweak on the computer and games. I also Enjoy the outdoors. Im a big gym enthusiast lifthing running just keeping the body moving. Ive also trained in boxing, kick boxing, Muaythai and a littl bit of jujitsu </p>
-        <img class="right" src="images/me.png" alt="Leo">
-
-        <img class="left" src="images/wamp.png" alt="wamp">
-
-        <img class="center" src="images/wamp_error.png" alt="error">
-
-
-</main>
-
-
-<aside>
-    <h2>weekly class exercies</h2>
-<h3>week2</h3>
-<ul>
-
-<li><a href="weeks/week2/var.php">var.php</a></li>
-<li><a href="weeks/week2/var2.php">var2.php</a></li>
-<li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
-<li><a href="weeks/week2/currency.php">currency.php</a></li>
-
-</ul>
-
-
-</aside>
-
-
-
-
-
+<?php
+include('./includes/header.php');
+?>
+<div id="wrapper">
+    
+<div id="hero">
+<img src="images/adonis the poodle.png" alt="Adonis the poddle">    
 </div>
+    <!--end hero-->
+        <main> <h1>Welcome to our Web App Programing class!</h1>
+        <h2>We are going to learn PHP!</h2>
+        <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </p>
+        <h2>Another Headerline 2!</h2>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    
+        </p>
+    
+    </main>
+<aside>
+        <h3>This is our headline three for our beautiful aside</h3>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+</aside>
+    
+    </div>
+    <!--end wrapper-->
+    
+    <?php
+    include('./includes/footer.php');
 
-
-  
-</body>
-</html>
